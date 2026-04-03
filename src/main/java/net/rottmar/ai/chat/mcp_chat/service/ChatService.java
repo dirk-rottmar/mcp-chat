89bab637aa12625c2fce26b0b25440eb1d8c598f
Dirk Rottmar <dirk@rottmar.net>
2026-04-03 19:28:30 +0700

    added math-tools-mcp endpoint

--- src/main/java/net/rottmar/ai/chat/mcp_chat/service/ChatService.java
+++ src/main/java/net/rottmar/ai/chat/mcp_chat/service/ChatService.java
@@ -14,7 +14,22 @@ public class ChatService {
     }
 
     public String ask(String question, String conversationId) {
+
+        var mathPromptExtension = """
+                You are a precise assistant.
+                When using the math tool, just provide the final result in plain text.
+                Do not generate spaces in numbers.
+                Do not use LaTeX or formulas.
+                Use the math tool even when using other tools and asked for calculations.
+                Whenever a user asks a question involving numbers, subtractions, additions,
+                or any quantities (e.g., '10 persons less 8'), you MUST call the 'mcp-math-tool'.
+                Do not provide an answer until you have received the result from the tool.
+                """;
+
+
+
         return chatClient.prompt()
+                .system(mathPromptExtension)
                 .user(question)
                 .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, conversationId))
                 .call()
